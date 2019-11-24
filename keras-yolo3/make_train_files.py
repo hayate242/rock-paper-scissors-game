a76d737c7eccfe23db22933db743058badfd68d1
中山颯 <sippu242@gmail.com>
2019-11-24 14:47:08 +0900

    change directry

--- keras-yolo3/make_train_files.py
+++ keras-yolo3/make_train_files.py
@@ -7,29 +7,33 @@ print(pwd)
 
 
 def make_train_files():
-    suffixs = ['_train','_val','_test']
+    suffixs = ['_train', '_val', '_test']
 
     for suffix in suffixs:
-        print('suffix',suffix)
-        new_file = open('{}.txt'.format(suffix.replace('_','')),'w')
+        print('suffix', suffix)
+        new_file = open('{}.txt'.format(suffix.replace('_', '')), 'w')
         text = ""
         for file in os.listdir():
-            if file.find(suffix) == -1:continue
+            if file.find(suffix) == -1:
+                continue
             with open(file) as f:
-                if text == "":text = f.read()
-                text =text +'\n'+ f.read()
+                if text == "":
+                    text = f.read()
+                text = text + '\n' + f.read()
         new_file.write(text)
 
-def split_val_test(rate:float):
+
+def split_val_test(rate):
     val = pandas.read_csv('val.txt')
     val = val.sample(frac=1)
-    print('len',len(val))
+    print('len', len(val))
     split = int(len(val) * rate)
     test = val.values
-    val[:split].to_csv('val.txt',index=False)
-    val[split:].to_csv('test.txt',index=False)
-    print('val.txt',split)
-    print('test.txt',len(val) - split)
+    val[:split].to_csv('val.txt', index=False)
+    val[split:].to_csv('test.txt', index=False)
+    print('val.txt', split)
+    print('test.txt', len(val) - split)
+
 
 if __name__ == '__main__':
     val_rate = 0.33
